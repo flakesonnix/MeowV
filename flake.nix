6836a415c8479440e4bd50e1893b0a9eb764b4d4
Lucy <lucy@example.com>
2026-05-23 15:15:11 +0200

    chore: add nix dev shell

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "MeowV standalone multiplayer prototype";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            rustToolchain
+            pkgs.pkg-config
+          ];
+
+          RUST_BACKTRACE = "1";
+        };
+      });
+}
